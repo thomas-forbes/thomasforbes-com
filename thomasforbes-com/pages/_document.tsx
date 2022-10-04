6d553ebc466c06c30524198159704aecc925385e
Folded-Code <thomas.reilly.forbes@gmail.com>
2022-10-04 12:50:27 +0100

    Setup new site

---
+++ thomasforbes-com/pages/_document.tsx
@@ -0,0 +1,14 @@
+import { Head, Html, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html>
+      <Head></Head>
+      <body>
+        <title>Upwritr</title>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
---
+++ thomasforbes-com/pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return (
+    <div className="w-full h-full flex flex-column items-center">
+      <h1 className="text-center">Thomas Forbes</h1>
+    </div>
+  )
+}
+
+export default Home
