76a1c880c08bee6489b43ef1d899a4787b7bbfe3
Folded-Code <thomas.reilly.forbes@gmail.com>
2022-10-27 17:34:36 +0100

    Added splitbee

--- next.config.js
+++ next.config.js
@@ -22,6 +22,18 @@ const nextConfig = withMDX({
   images: {
     domains: ['allmycontact.info'],
   },
+  async rewrites() {
+    return [
+      {
+        source: '/bee.js',
+        destination: 'https://cdn.splitbee.io/sb.js',
+      },
+      {
+        source: '/_hive/:slug',
+        destination: 'https://hive.splitbee.io/:slug',
+      },
+    ]
+  },
 })
 
 module.exports = nextConfig
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,6 @@
+import splitbee from '@splitbee/web'
 import type { AppProps } from 'next/app'
+import { useEffect } from 'react'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import '../styles/globals.css'
 import '../styles/nightOwl.css'
@@ -6,6 +8,12 @@ import '../styles/nightOwl.css'
 const queryClient = new QueryClient()
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    splitbee.init({
+      scriptUrl: '/bee.js',
+      apiUrl: '/_hive',
+    })
+  }, [])
   return (
     <QueryClientProvider client={queryClient}>
       <Component {...pageProps} />
