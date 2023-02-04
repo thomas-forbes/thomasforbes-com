a6cc445a874c8fbb1547e32997a5beae564db0e2
Folded-Code <34174315+Folded-Code@users.noreply.github.com>
2023-02-04 18:36:47 +0000

    fixed dumb error

--- src/utils/getArticles.ts
+++ src/utils/getArticles.ts
@@ -4,12 +4,11 @@ import { Article } from './types'
 
 export default async function getArticles(): Promise<Article[]> {
   const articleDirectory = path.join(process.cwd(), 'src/pages/blog')
+  const files = await fs.promises.readdir(articleDirectory)
   const articles = (
     await Promise.all(
-      fs.readdirSync(articleDirectory).map(async (filePath) => ({
-        module: await import(
-          path.join(process.cwd(), `src/pages/blog/${filePath}`)
-        ),
+      files.map(async (filePath) => ({
+        module: await import(`../pages/blog/${filePath}`),
         link: '/blog/' + filePath.replace('.mdx', ''),
       }))
     )
