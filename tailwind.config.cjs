e38daf7a14453dfa01c160fd3f2c56d9eddf6bf9
thomas-forbes <34174315+thomas-forbes@users.noreply.github.com>
2023-04-08 12:45:38 +0100

    major

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        serif: ['Bitter', ...defaultTheme.fontFamily.serif],
+        'sans-serif': ['Nunito', ...defaultTheme.fontFamily.sans],
+        sans: ['Ubuntu', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 }
