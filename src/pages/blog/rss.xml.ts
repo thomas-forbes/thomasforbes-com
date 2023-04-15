608ee157fe2f456e499d3575e20204ef8cb79c7e
thomas-forbes <34174315+thomas-forbes@users.noreply.github.com>
2023-04-15 17:28:36 +0100

    improvements

--- src/pages/blog/rss.xml.ts
+++ src/pages/blog/rss.xml.ts
@@ -1,6 +1,10 @@
 import rss from '@astrojs/rss'
-import { getCollection } from 'astro:content'
 import type { APIContext } from 'astro'
+import { getCollection } from 'astro:content'
+import MarkdownIt from 'markdown-it'
+import sanitizeHtml from 'sanitize-html'
+
+const parser = new MarkdownIt()
 
 export async function get(context: APIContext) {
   const posts = await getCollection('blog')
@@ -10,6 +14,7 @@ export async function get(context: APIContext) {
     site: context.site!.href,
     items: posts.map((post) => ({
       ...post.data,
+      content: sanitizeHtml(parser.render(post.body)),
       link: `/blog/${post.slug}/`,
     })),
   })
