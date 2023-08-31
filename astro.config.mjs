518fd8c0f9f936777ef68784929bc5dc657eac4d
thomas-forbes <34174315+thomas-forbes@users.noreply.github.com>
2023-08-31 16:27:36 -0700

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,17 @@
-import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
+import { defineConfig } from 'astro/config'
 
 import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://thomasforbes.com',
-  integrations: [mdx(), sitemap(), tailwind()],
+  integrations: [
+    mdx(),
+    sitemap(),
+    tailwind({
+      config: { applyBaseStyles: false },
+    }),
+  ],
 })
