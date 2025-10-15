6567f1676d9ac3b3daeb84d9aaa11495bfdc8811
thomas-forbes <34174315+thomas-forbes@users.noreply.github.com>
2025-10-15 11:34:23 +0100

    privacy policy

---
+++ src/app/(page)/privacy-policy/page.tsx
@@ -0,0 +1,9 @@
+import { Section } from '@/components/ui/section';
+
+export default function PrivacyPolicy() {
+  return (
+    <Section title="Privacy Policy" as="h2">
+      <p>No data is collected.</p>
+    </Section>
+  );
+}
